d7e9fe86deec674ea35550195afc2961d40923b0
Marcos Monteiro <mrcsmonteiro@gmail.com>
2025-09-18 12:43:45 +1000

    Update region and add use_lockfile parameter

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,15 +1,15 @@
 terraform {
   backend "s3" {
-    bucket         = "tfstate-wiki.js"
-    key            = "terraform/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table-wiki.js"
-    encrypt        = true
+    bucket       = "tfstate-wiki.js"
+    key          = "terraform/terraform.tfstate"
+    region       = var.aws_region
+    use_lockfile = true
+    encrypt      = true
   }
 
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.13.0"
     }
   }
